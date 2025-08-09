7d126d93c52e05a9f7e0501aff49a682d1c0bf46
Intronox <intronox0@gmail.com>
2025-08-09 19:57:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,8 +29,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1214348787")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kiyotoprotocol01:kiyotoprotocol01@cluster0.inftpwu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kiyotoprotocol01:kiyotoprotocol01@cluster0.inftpwu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "HentaiDb")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "0"))  # 0 means no expiry
